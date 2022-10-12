b6eacbab3c591dc83c5ab029eaf71d474d40425a
Marek Wypich <marekwypich@gmail.com>
2022-10-12 17:27:29 -0400

    PSKModulator first working version

--- examples/exampleModulator.cpp
+++ examples/exampleModulator.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include "PSKModulator.h"
+#include "../lib/PSKModulator.h"
 
 int main(){
-    PSKModulator modulator(4);
-    std::cout << "Modulation oreder = " << modulator.modulationOrder << std::endl;
-    std::vector<int> x{3, 1, 0, 3, 1, 2, 0};
+    PSKModulator modulator(8);
+    std::vector<int> x{0, 1, 2, 3, 4, 5, 6, 7};
     std::vector<comp> y = modulator.modulate(x);
     for (int i = 0; i < x.size(); i++){
         std::cout << x[i] << ": " << real(y[i]) << " + i * " << imag(y[i]) <<std::endl;
