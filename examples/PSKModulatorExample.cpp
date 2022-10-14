c6c015002660ecbdac1339d5c973e0a6ea7fc7fa
Marek Wypich <marekwypich@gmail.com>
2022-10-14 05:59:31 -0400

    PSKModulatorExample updated with Makefile: plots added

--- examples/PSKModulatorExample.cpp
+++ examples/PSKModulatorExample.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
 #include "../lib/PSKModulator.h"
-//#include "matplotlibcpp.h"
+#include "matplotlib-cpp.h"
+
+namespace plt = matplotlibcpp;
 
 int main(){
     PSKModulator modulator(8, 15);
     std::vector<int> x{0, 1, 2, 3, 4, 5, 6, 7};
     std::vector<comp> y = modulator.modulate(x);
-    for (int i = 0; i < x.size(); i++){
-        std::cout << x[i] << ": " << real(y[i]) << " + i * " << imag(y[i]) <<std::endl;
+
+    std::vector<double> r;
+    std::vector<double> i;
+
+    for (auto number : y){
+        r.push_back(real(number));
+        i.push_back(imag(number));
     }
+
+    double rMax = *std::max_element(r.begin(), r.end()) + 1;
+    double rMin = *std::min_element(r.begin(), r.end()) - 1;
+
+    double iMax = *std::max_element(i.begin(), i.end()) + 1;
+    double iMin = *std::min_element(i.begin(), i.end()) - 1;
+
+    std::vector<double> xAxisX{rMin, rMax};
+    std::vector<double> xAxisY{0, 0};
+
+    std::vector<double> yAxisX{0, 0};
+    std::vector<double> yAxisY{iMin, iMax};
+
+    plt::figure();
+    plt::scatter(r, i);
+    plt::plot(xAxisX, xAxisY);
+    plt::plot(yAxisX, yAxisY);
+    plt::show();
+
     return 0;
 }
